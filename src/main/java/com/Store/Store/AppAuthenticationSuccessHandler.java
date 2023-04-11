7a6758359c5965145036c1ec85435a2c2f427c73
keylo <keylo@DESKTOP-H02GLLS>
2023-04-11 14:26:32 -0600

    Ahora si esta bueno

--- src/main/java/com/Store/Store/AppAuthenticationSuccessHandler.java
+++ src/main/java/com/Store/Store/AppAuthenticationSuccessHandler.java
@@ -4,10 +4,10 @@
  */
 package com.Store.Store;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
